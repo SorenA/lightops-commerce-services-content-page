8c83db266ab5f0ba26376b7d2522ac155562ac4c
SorenA <s.dev@lysdal.org>
2020-06-24 20:28:49 +0200

    Add CQRS based health check service.

---
+++ src/LightOps.Commerce.Services.ContentPage/Api/Services/IHealthService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace LightOps.Commerce.Services.ContentPage.Api.Services
+{
+    public interface IHealthService
+    {
+        Task<HealthStatus> CheckContentPage();
+    }
+}
